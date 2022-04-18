ac9455e67d5c0813e913a56f62c9737195641b80
Nguyễn Văn Bắc <cuocsonglala@gmail.com>
2022-04-18 22:18:33 +0700

    fix validate error

---
+++ UniAdmissionPlatform.BusinessTier/ViewModels/SlotViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UniAdmissionPlatform.BusinessTier.ViewModels
+{
+    public class SlotFilterForSchoolAdmin
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? Status { get; set; }
+    }
+    public class SlotViewModel
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? HighSchoolId { get; set; }
+        public int? Status { get; set; }
+    }
+}
