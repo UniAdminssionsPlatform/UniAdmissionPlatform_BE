1430122391c922dbeff7e3a29003b0b9aa34c747
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-19 17:52:24 +0700

    Test

--- UniAdmissionPlatform.WebApi/Startup.cs
+++ UniAdmissionPlatform.WebApi/Startup.cs
@@ -97,15 +97,32 @@ namespace UniAdmissionPlatform.WebApi
             // }
             //config to deploy
             app.UseForwardedHeaders();
-
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.IsHttps || context.Request.Headers["X-Forwarded-Proto"] == Uri.UriSchemeHttps)
+                {
+                    await next();
+                }
+                else
+                {
+                    string queryString = context.Request.QueryString.HasValue ? context.Request.QueryString.Value : string.Empty;
+                    var https = "https://" + context.Request.Host + context.Request.Path + queryString;
+                    context.Response.Redirect(https);
+                }
+            });
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // code removed for clarity
             }
             else
             {
+                // code removed for clarity
                 app.UseHsts();
             }
+            app.UseHttpsRedirection();
+            // code removed for clarity
+            app.UseCertificateForwarding();
+            app.UseMvc();
             //end
             app.UseSwagger(c =>
             {
