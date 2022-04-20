158d1d3a9a9829c03ccf1da92158cd36429973a5
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2022-04-20 21:29:56 +0700

    [FIX] CI CD (#18)

--- UniAdmissionPlatform.WebApi/Startup.cs
+++ UniAdmissionPlatform.WebApi/Startup.cs
@@ -30,43 +30,51 @@ namespace UniAdmissionPlatform.WebApi
         }
 
         public IConfiguration Configuration { get; }
-        
+
         private const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //config production
+            services.AddControllersWithViews();
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto;
+            });
+            //end
             services.AddCors(o => o.AddPolicy(MyAllowSpecificOrigins, builder =>
             {
                 builder.AllowAnyOrigin()
                     .AllowAnyMethod()
                     .AllowAnyHeader();
             }));
-            
+
             services.InitSwagger();
 
-            
+
             services.AddSingleton<ICasbinService, CasbinService>();
-            
+
             services.ConfigureJsonFormatServices();
 
             services.InitFirebase();
-            
+
             services.AddRouting(options => options.LowercaseUrls = true);
-            
+
             services.ConfigureVersioningServices();
 
             services.ConfigureFilterServices();
 
             services.InitializerDI();
-            
+
             services.ConfigureAutoMapperServices();
 
             services.AddDbContext<db_uapContext>(
                 opt => opt.UseMySQL(Configuration["ConnectionStrings:DbContext"]));
 
             services.AddSingleton<IAuthService, AuthService>();
-            
+
             services.AddSingleton<IFirebaseStorageService, FirebaseStorageService>();
 
             services.ConfigureAutoMapperServices();
@@ -75,19 +83,26 @@ namespace UniAdmissionPlatform.WebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IApiVersionDescriptionProvider provider)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UniAdmissionPlatform.WebApi v1"));
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseForwardedHeaders();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseForwardedHeaders();
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            //end
             app.UseSwagger(c =>
             {
                 c.RouteTemplate = "/swagger/{documentName}/swagger.json";
             });
-            
+
             app.UseSwaggerUI(c =>
             {
                 foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
@@ -98,20 +113,14 @@ namespace UniAdmissionPlatform.WebApi
                 c.IndexStream = () => GetType().Assembly.GetManifestResourceStream("UniAdmissionPlatform.WebApi.Resources.Swagger.index.html");
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "UniAdmissionPlatform.WebApi v1");
             });
-
             app.UseDeveloperExceptionPage();
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-            
             app.UseMiddleware<JwtMiddleware>();
             app.UseMiddleware<ExceptionMiddleware>();
-            
+
             app.UseCors(MyAllowSpecificOrigins);
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
-
     }
 }
