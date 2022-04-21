4256b79a8a96122e01c55e3f2015c917bf8c66bf
Nguyen Van Bac <khunglongtientien@gmail.com>
2022-04-21 13:00:12 +0700

    feat: update avatar

---
+++ UniAdmissionPlatform.BusinessTier/Commons/Toolkit/FileToolKit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using UniAdmissionPlatform.BusinessTier.Responses;
+
+namespace UniAdmissionPlatform.BusinessTier.Commons.Toolkit
+{
+    public static class FileToolKit
+    {
+        public static void ValidateFileName(IFormFile file, string[] extensions, long fileMaxSize)
+        {
+            var lastIndexOf = file.FileName.LastIndexOf(".", StringComparison.Ordinal);
+            
+            if (lastIndexOf == -1 || !extensions.Contains(file.FileName[lastIndexOf..file.FileName.Length].ToLower()))
+            {
+                throw new ErrorResponse((int)HttpStatusCode.BadRequest, "Định dạng file bắt buộc là " + string.Join(" hoặc ",extensions) );
+            }
+
+            if (file.Length > fileMaxSize)
+            {
+                throw new ErrorResponse((int) HttpStatusCode.BadRequest, "Dung lượng file phải bé hơn 10 MB");
+            }
+        }
+    }
+}
--- UniAdmissionPlatform.WebApi/Controllers/AccountsController.cs
+++ UniAdmissionPlatform.WebApi/Controllers/AccountsController.cs
@@ -1,7 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UniAdmissionPlatform.BusinessTier.Commons.Enums;
+using UniAdmissionPlatform.BusinessTier.Commons.Toolkit;
 using UniAdmissionPlatform.BusinessTier.Generations.Services;
+using UniAdmissionPlatform.BusinessTier.Responses;
+using UniAdmissionPlatform.BusinessTier.Services;
 using UniAdmissionPlatform.BusinessTier.ViewModels;
+using UniAdmissionPlatform.WebApi.Attributes;
+using UniAdmissionPlatform.WebApi.Helpers;
 
 namespace UniAdmissionPlatform.WebApi.Controllers
 {
@@ -9,11 +19,15 @@ namespace UniAdmissionPlatform.WebApi.Controllers
     [ApiController]
     public class AccountsController : ControllerBase
     {
+        private readonly IAuthService _authService;
         private readonly IAccountService _accountService;
+        private readonly IFirebaseStorageService _firebaseStorageService;
 
-        public AccountsController(IAccountService accountService)
+        public AccountsController(IAccountService accountService, IFirebaseStorageService firebaseStorageService, IAuthService authService)
         {
             _accountService = accountService;
+            _firebaseStorageService = firebaseStorageService;
+            _authService = authService;
         }
 
         [HttpGet("get-students-info")]
@@ -21,5 +35,58 @@ namespace UniAdmissionPlatform.WebApi.Controllers
         {
             return Ok(await _accountService.GetAll(filter, page, limit, sort));
         }
+
+        [HttpPost("update-avatar")]
+        [CustomAuthorize]
+        public async Task<IActionResult> UpdateAvatar([Required] IFormFile file)
+        {
+            try
+            {
+                FileToolKit.ValidateFileName(file, new[] { ".jpg", ".png", ".jpeg" }, FileMaxSize);
+            }
+            catch (ErrorResponse e)
+            {
+                throw e.Error.Code switch
+                {
+                    (int)HttpStatusCode.BadRequest => new GlobalException(ExceptionCode.PrintMessageErrorOut,
+                        "Đổi ảnh đại diện thất bại. " + e.Error.Message),
+                    _ => new GlobalException(ExceptionCode.PrintMessageErrorOut, e.Error.Message)
+                };
+            }
+
+            string fileUrl;
+            try
+            {
+                var lastIndexOf = file.FileName.LastIndexOf(".", StringComparison.Ordinal);
+                fileUrl = await _firebaseStorageService.UploadImage(file.FileName[lastIndexOf..file.FileName.Length].ToLower(),"avatar",file.OpenReadStream());
+            }
+            catch (ErrorResponse e)
+            {
+                throw e.Error.Code switch
+                {
+                    _ => new GlobalException(ExceptionCode.PrintErrorObjectOut, e.Error.Message)
+                };
+            }
+
+
+            var userId = _authService.GetUserId(HttpContext);
+            try
+            {
+                await _accountService.UploadAvatar(userId, fileUrl);
+
+                return Ok(MyResponse<object>.OkWithMessage("Đổi ảnh đại diện thành công."));
+            }
+            catch (ErrorResponse e)
+            {
+                throw e.Error.Code switch
+                {
+                    (int)HttpStatusCode.NotFound => new GlobalException(ExceptionCode.PrintMessageErrorOut,
+                        "Đổi ảnh đại diện thất bại. " + e.Error.Message),
+                    _ => new GlobalException(ExceptionCode.PrintMessageErrorOut, e.Error.Message)
+                };
+            }
+        }
+        
+        private const long FileMaxSize = 10000000;
     }
 }
