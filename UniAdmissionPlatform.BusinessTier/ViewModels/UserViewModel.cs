418b49776e9da1202a6d0b29c170fe26537b958e
Nguyễn Văn Bắc <cuocsonglala@gmail.com>
2022-05-02 23:16:41 +0700

    UAP-130: BE GET LIST HIGH SCHOOL MANAGER STATUS PENDING

---
+++ UniAdmissionPlatform.BusinessTier/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UniAdmissionPlatform.BusinessTier.Commons.Attributes;
+
+namespace UniAdmissionPlatform.BusinessTier.ViewModels
+{
+    public class UserBaseViewModel
+    {
+        public int? Id { get; set; }
+        [String]
+        public string Uid { get; set; }
+        public int? Status { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
