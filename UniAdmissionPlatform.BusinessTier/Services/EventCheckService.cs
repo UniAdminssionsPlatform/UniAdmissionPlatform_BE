cd8734dde59414cbd858c4b8b53767f80ad46048
Hao <haodngn@gmail.com>
2022-05-11 00:43:38 +0700

    UAP-150: [BE] - Implement reject event highschool API.

--- UniAdmissionPlatform.BusinessTier/Services/EventCheckService.cs
+++ UniAdmissionPlatform.BusinessTier/Services/EventCheckService.cs
@@ -20,7 +20,7 @@ namespace UniAdmissionPlatform.BusinessTier.Generations.Services
     {
         Task ApproveEventToSlot(int highSchoolId, int eventCheckId);
         Task<EventBySlotBaseViewModel> GetEventBySlotId(int slotId);
-
+        Task RejectEventToSlot(int highSchoolId, int eventCheckId);
         Task<PageResult<EventWithSlotViewModel>> GetEventsByHighSchoolId(int highSchoolId, string sort, int page,
             int limit);
     }
@@ -99,6 +99,73 @@ namespace UniAdmissionPlatform.BusinessTier.Generations.Services
             }
 
             eventCheck.Status = (int)EventCheckStatus.Approved;
+            await UpdateAsyn(eventCheck);
+        }
+        
+        public async Task RejectEventToSlot(int highSchoolId, int eventCheckId)
+        {
+            var eventCheck = await Get(ec => ec.Id == eventCheckId)
+                .Include(ec => ec.Slot)
+                .Include(ec => ec.Event)
+                .FirstOrDefaultAsync();
+            
+            if (eventCheck.Slot.HighSchoolId != highSchoolId)
+            {
+                throw new ErrorResponse(StatusCodes.Status400BadRequest, "Cuộc đặt lịch này không phải của trường bạn.");
+            }
+            
+            if (eventCheck == null)
+            {
+                throw new ErrorResponse(StatusCodes.Status404NotFound, "Không tìm thấy cuộc đặt lịch này.");
+            }
+
+            if (eventCheck.Status != (int)EventCheckStatus.Pending)
+            {
+                if (eventCheck.Status == (int)EventCheckStatus.Approved)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Cuộc đặt lịch này đã được chấp nhận.");
+                }
+                
+                if (eventCheck.Status == (int)EventCheckStatus.Reject)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Cuộc đặt lịch này đã bị từ chối.");
+                }
+            }
+
+            var uniEvent = eventCheck.Event;
+            if (uniEvent.DeletedAt != null)
+            {
+                throw new ErrorResponse(StatusCodes.Status404NotFound, "Không thể tìm thấy sự kiện trong cuộc đặt lịch");
+            }
+
+            if (uniEvent.Status != (int) EventStatus.OnGoing)
+            {
+                if (uniEvent.Status == (int) EventStatus.Cancel)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Sự kiện này đã bị hủy.");
+                }
+                
+                if (uniEvent.Status == (int) EventStatus.Done)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Sự kiện này đã tổ chức rồi.");
+                }
+            }
+
+            var slot = eventCheck.Slot;
+            if (slot.Status != (int) SlotStatus.Open)
+            {
+                if (slot.Status == (int) SlotStatus.Close)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Buổi này đã bị đóng.");
+                }
+                
+                if (slot.Status == (int) SlotStatus.Full)
+                {
+                    throw new ErrorResponse(StatusCodes.Status400BadRequest, "Buổi này đã bị đầy lịch.");
+                }
+            }
+
+            eventCheck.Status = (int)EventCheckStatus.Reject;
             await UpdateAsyn(eventCheck);
         }
         
