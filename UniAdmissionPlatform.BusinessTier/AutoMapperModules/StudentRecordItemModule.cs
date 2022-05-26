97405917ce7463416b72d22b3ca97526429d354d
Hao <haodngn@gmail.com>
2022-05-26 12:43:11 +0700

    UAP-103: [BE] - Implement crud student record item API

---
+++ UniAdmissionPlatform.BusinessTier/AutoMapperModules/StudentRecordItemModule.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using UniAdmissionPlatform.BusinessTier.Requests.StudentRecordItem;
+using UniAdmissionPlatform.BusinessTier.ViewModels;
+using UniAdmissionPlatform.DataTier.Models;
+
+namespace UniAdmissionPlatform.BusinessTier.AutoMapperModules
+{
+    public static class StudentRecordItemModule
+    {
+        public static void ConfigStudentRecordItemMapperModule(this IMapperConfigurationExpression mc)
+        {
+            mc.CreateMap<StudentRecordItem, StudentRecordItemBaseViewModel>();
+            mc.CreateMap<CreateStudentRecordItemRequest, StudentRecordItem>();
+            mc.CreateMap<UpdateStudentRecordItemRequest, StudentRecordItem>()
+                .ForAllMembers(opt => opt.Condition((src,des,srcMember)=> srcMember != null));
+        }
+    }
+}
---
+++ UniAdmissionPlatform.BusinessTier/Requests/StudentRecordItem/UpdateStudentRecordItemRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UniAdmissionPlatform.BusinessTier.Requests.StudentRecordItem
+{
+    public class UpdateStudentRecordItemRequest
+    {
+        public float? Score { get; set; }
+        public int? SchoolRecordId { get; set; }
+        public int? SubjectId { get; set; }
+    }
+}
