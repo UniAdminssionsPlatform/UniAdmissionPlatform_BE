fb105195da7dad919ad042d353164034a9ad380b
khung <khunglongtientien@gmail.com>
2022-06-09 22:47:26 +0800

    fe request 7-6

--- UniAdmissionPlatform.BusinessTier/Requests/StudentRecordItem/UpdateStudentRecordItemRequest.cs
+++ UniAdmissionPlatform.BusinessTier/Requests/StudentRecordItem/UpdateStudentRecordItemRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace UniAdmissionPlatform.BusinessTier.Requests.StudentRecordItem
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniAdmissionPlatform.BusinessTier.Requests.StudentRecordItem
 {
     public class UpdateStudentRecordItemRequest
     {
         public float? Score { get; set; }
         public int? SchoolRecordId { get; set; }
+        public int? SubjectId { get; set; }
+    }
+    
+    public class UpdateStudentRecordItemBase
+    {
+        public int Id { get; set; }
+        [Range(0, 10)]
+        public float? Score { get; set; }
         public int? SubjectId { get; set; }
     }
 }
