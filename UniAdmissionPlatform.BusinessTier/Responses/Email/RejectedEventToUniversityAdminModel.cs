850b760af17575d1ca88d3b53018b031cfbe5dab
Hao <haodngn@gmail.com>
2022-07-12 12:53:05 +0700

    UAP-151: [BE] - Send mail when event of university is rejected

---
+++ UniAdmissionPlatform.BusinessTier/Responses/Email/RejectedEventToUniversityAdminModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UniAdmissionPlatform.BusinessTier.Responses.Email
+{
+    public class RejectedEventToUniversityAdminModel
+    {
+        public string NameOfUniAdmin { get; set; }
+        public string NameOfHighSchool { get; set; }
+        public string EventName { get; set; }
+        public DateTime RejectedTime { get; set; }
+        public DateTime? SlotStartDate { get; set; }
+        public DateTime? SlotEndDate { get; set; }
+        public string BookingDetailUrl { get; set; }
+    }
+}
