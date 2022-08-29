83f99da81fc9a0ec4a2aa78994c5548e8398181a
Hao <haodngn@gmail.com>
2022-08-29 20:10:09 +0700

    off

--- UniAdmissionPlatform.WebApi/Startup.cs
+++ UniAdmissionPlatform.WebApi/Startup.cs
@@ -76,7 +76,7 @@ namespace UniAdmissionPlatform.WebApi
             
             JobStorage.Current = mySqlStorage;
             
-            services.AddHangfireServer(options => options.WorkerCount = 2);
+            //services.AddHangfireServer(options => options.WorkerCount = 2);
             
             services.InitFirebase();
 
